37e4371c77d00e7cb8a2e70d7f9debc2761284c4
Dara <58168757+DaraNel@users.noreply.github.com>
2020-03-25 01:28:04 -0700

    Add files via upload
    Javascript Task 1

---
+++ index.js
@@ -0,0 +1,11 @@
+let name = "Dara Nelson,(DaraNel)";
+console.log("My name is: " + name);
+let courses = "[HTML, CSS, JavaScript, & Design]";
+console.log("My courses are: "+ courses)    
+const data = [];    
+for (let i=2; i<=200; i++) {
+    if (i%2===0) {
+        data.push(i);
+    }
+}
+console.log(data);
